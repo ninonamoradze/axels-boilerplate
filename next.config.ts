11eccaa7fa519ca903c10b8a9adb2ee623645681
StartupGeorgia <info@startupgeorgia.org>
2026-05-17 13:34:20 +0400

    ci: deploy functionality-Nino to /ninonamoradze

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["172.29.179.74"],
+  basePath: '/ninonamoradze',
+  assetPrefix: '/ninonamoradze',
 };
 
 export default nextConfig;
